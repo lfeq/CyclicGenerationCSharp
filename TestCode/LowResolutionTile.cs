b8774eb82422972a393efb0477cb2f66f99a2ffb
Lorenz Equihua <panchocosa@gmail.com>
2024-09-17 17:43:38 -0600

    added documentation to LowResolutionTile.cs

--- TestCode/LowResolutionTile.cs
+++ TestCode/LowResolutionTile.cs
@@ -1,19 +1,45 @@
 namespace TestCode.Graphs;
 
+/// <summary>
+/// Represents a tile in a low-resolution tilemap.
+/// </summary>
 public class LowResolutionTile {
+    /// <summary>
+    /// The type of the tile (e.g., Room, Door, Empty).
+    /// </summary>
     public LowResolutionTileType tileType = LowResolutionTileType.None;
+    
+    /// <summary>
+    /// The position of the tile in the tilemap.
+    /// </summary>
     public Vector2 position;
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="LowResolutionTile"/> class based on a node and its position.
+    /// </summary>
+    /// <param name="t_node">The node from which the tile type is derived.</param>
+    /// <param name="t_position">The position of the tile in the tilemap.</param>
     public LowResolutionTile(Node t_node, Vector2 t_position) {
         tileType = convertNodeTypeToLowResolutionTileType(t_node.NodeType);
         position = t_position;
     }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="LowResolutionTile"/> class based on a tile type and its position.
+    /// </summary>
+    /// <param name="t_tileType">The type of the tile.</param>
+    /// <param name="t_position">The position of the tile in the tilemap.</param>
     public LowResolutionTile(LowResolutionTileType t_tileType, Vector2 t_position) {
         tileType = t_tileType;
         position = t_position;
     }
 
+    /// <summary>
+    /// Converts a node type to a corresponding low-resolution tile type.
+    /// </summary>
+    /// <param name="t_nodeType">The node type to convert.</param>
+    /// <returns>The corresponding <see cref="LowResolutionTileType"/>.</returns>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown when an unsupported node type is provided.</exception>
     private LowResolutionTileType convertNodeTypeToLowResolutionTileType(NodeType t_nodeType) {
         switch (t_nodeType) {
             case NodeType.None:
@@ -35,6 +61,10 @@ public class LowResolutionTile {
         }
     }
     
+    /// <summary>
+    /// Returns a string representation of the tile.
+    /// </summary>
+    /// <returns>A string that represents the tile type.</returns>
     public override string ToString() {
         switch (tileType) {
             case LowResolutionTileType.None:
@@ -51,6 +81,9 @@ public class LowResolutionTile {
     }
 }
 
+/// <summary>
+/// Enumeration representing the type of a low-resolution tile.
+/// </summary>
 public enum LowResolutionTileType {
     None,
     Room,
